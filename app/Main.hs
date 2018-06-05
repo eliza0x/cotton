cba58e3ffe2b3857bc26ec701f04e256773d3b4d
eliza0x <me@eliza.link>
2018-06-06 03:32:10 +0900

    update: change output formats

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,10 @@
 module Main where
 
 import qualified Cotton as C
-import Control.Lens
 import Options.Declarative
 import Control.Monad.IO.Class
 import qualified System.Process as Sys
+import qualified System.Exit    as E
 import qualified Data.Text      as T
 import qualified Data.Text.IO   as T
 
@@ -15,9 +15,11 @@ import qualified Data.Text.IO   as T
 compile :: Bool -> String -> IO ()
 compile isDebugMode filepath = do
     llvmir <- C.compile isDebugMode =<< readFile filepath
-    T.putStr "output: "
-    print . (^. _1) =<< 
-        Sys.readCreateProcessWithExitCode (Sys.shell "lli") (T.unpack llvmir)
+    (exitCode, _, _) <- Sys.readCreateProcessWithExitCode (Sys.shell "lli") (T.unpack llvmir)
+    case exitCode of
+        E.ExitFailure n -> print n
+        _               -> print 0
+        
 
 debugSubCmd :: Arg "FILEPATH" String
             -> Cmd "execute source code (verbose)" ()
